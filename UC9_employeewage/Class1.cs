d50096024383d83acfeda7c917f751ea0bcd7d10
shashiRgowda <shashikumartr64@gmail.com>
2021-05-20 21:24:45 +0530

    Added:UC9_employeewage

---
+++ UC9_employeewage/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageBuilderObject
+{
+    class class1
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("dMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+    }
+}
