84b7d94dbc6a64c42f89a5953a60f46d2efe42fd
shashiRgowda <shashikumartr64@gmail.com>
2021-05-20 20:57:42 +0530

    Added:UC1_EmployeePresent

---
+++ UC1_EmployeePresent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmpCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present!");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+        }
+    }
+}
